3bbe6f45c7b1463bcd205a4b7291ac1c99068706
gbh10c <grandcasa10@gmail.com>
2022-05-14 11:05:20 -0400

    Updated Prettier settings and added custom Class HTML tag

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,9 +1,12 @@
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
+import React from 'react';
 
-function Expenses(props) {
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -24,7 +27,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
