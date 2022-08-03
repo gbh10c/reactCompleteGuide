f0a4d6a1a43c6b156d3fe06de8c985677fce6ef5
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-02 23:47:12 -0400

    Clear form onSubmit

--- forms-project/src/components/SimpleInput.js
+++ forms-project/src/components/SimpleInput.js
@@ -12,16 +12,25 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     console.log(enteredName);
-    
+
     const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue)
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; NOT IDEAL, DON'T MANIPULATE THE DOM
+    setEnteredName('');
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className='form-actions'>
         <button>Submit</button>
